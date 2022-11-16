8397ea40e20cf3666166326e6a5ad73eb8863ad5
Khasheef <khasheef.abrahams@yahoo.com>
2022-11-16 14:28:30 -0800

    Lists

--- getline.c
+++ getline.c
@@ -221,7 +221,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 
 		return (-1);
 
-	c = _strchr(buf + i, '\n');
+	c = _strnchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
 
